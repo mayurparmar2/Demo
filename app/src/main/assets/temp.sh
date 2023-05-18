fb95acb8fcf19270a4c13340c8c91692daa4fa3d
Mayur Parmar <mayurparmar6000@gmail.com>
2023-05-19 00:50:53 +0530

    Update temp.sh

--- app/src/main/assets/temp.sh
+++ app/src/main/assets/temp.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 # Demo-master
-echo "Enter the file name to delete: "
-read directory
 
-directories=(
-"C:/Users/Mayur/Desktop/MyProjects/$directory/app/src/main/res/menu"
-)
+Get-ChildItem -Recurse -Filter "*.xml" | ForEach-Object {
+  $content = Get-Content $_.FullName
+  $newContent1 = $content -replace '<(\w+)\s+[^>]*name="common_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent2 = $newContent1 -replace '<(\w+)\s+[^>]*name="m3_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent3 = $newContent2 -replace '<(\w+)\s+[^>]*name="bright_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent4 = $newContent3 -replace '<(\w+)\s+[^>]*name="cardview_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent5 = $newContent4 -replace '<(\w+)\s+[^>]*name="design_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent6 = $newContent5 -replace '<(\w+)\s+[^>]*name="dim_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent7 = $newContent6 -replace '<(\w+)\s+[^>]*name="foreground_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent8 = $newContent7 -replace '<(\w+)\s+[^>]*name="error_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent9 = $newContent8 -replace '<(\w+)\s+[^>]*name="mtrl_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent10 = $newContent9 -replace '<(\w+)\s+[^>]*name="primary_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent11 = $newContent10 -replace '<(\w+)\s+[^>]*name="ripple_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent12 = $newContent11 -replace '<(\w+)\s+[^>]*name="secondary_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent13 = $newContent12 -replace '<(\w+)\s+[^>]*name="tooltip_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent14 = $newContent13 -replace '<(\w+)\s+[^>]*name="switch_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent15 = $newContent14 -replace '<(\w+)\s+[^>]*name="foreground_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent16 = $newContent16 -replace '<(\w+)\s+[^>]*name="material_[^"]*"[^>]*>(.*?)<\/\1>', {  }
+  $newContent16 | Set-Content $_.FullName
+}
 
-# Iterate through the directories
-for dir in "${directories[@]}"; do
-  # Check if the directory exists
-  if [[ -d "$dir" ]]; then
-    # Find and delete files starting with 'abc' within the directory and its subdirectories
-    for file in $(find "$dir" -type f -name 'menu*'); do
-      echo "Deleting file: $file"
-      rm "$file"
-    done
-    echo "Deleted files starting with 'abc' within directory: $dir"
-  else
-    echo "Directory not found: $dir"
-  fi
-done
-
-#if [ -f "$filename" ]; then
-  #  rm "$filename"
-   # echo "File '$filename' deleted successfully."
-#else
-#    echo "File '$filename' does not exist."
-#fi
 
 
 
