4e0249ce9b4b917dbc4418b6474da2b2da7280e9
theparmarmayur <mayurparmar6000@gmail.com>
2023-05-29 11:50:23 +0530

    adding  29 may  adding java.sh

--- app/src/main/assets/Resources_imprt2.sh
+++ app/src/main/assets/Resources_imprt2.sh
@@ -5,6 +5,15 @@ JAVA_SRC_PATH="$ANDROID_PROJECT_PATH/app/src/main/java"
 Jadx_RES_PATH="F:/SaveJadx/$ProjectName/resources/res"
 RES_PATH="$ANDROID_PROJECT_PATH/app/src/main/res"
 MAIN="$ANDROID_PROJECT_PATH/app/src/main"
+
+## Copy the file
+#appIcon="C:/AndroidProject/GitHubDemo/app/src/main/res/drawable/icon200.png"
+#appIconDestinationDir="C:/AndroidProject/Test/$ProjectName/app/src/main/res/drawable"
+#if [ ! -d "$appIconDestinationDir" ]; then
+#  mkdir -p "$appIconDestinationDir"
+#fi
+#cp "$appIcon" "$appIconDestinationDir"
+
 directorieslist=(
   "values"
   "layout"
@@ -35,12 +44,12 @@ copyFile() {
 fun_child() {
   resource_type="$1"
   search_path="$2"
-#  matches=($(grep -Eo '@'$resource_type'/[A-Za-z0-9_]+' "$search_path"))
-  list_names=($(grep -Eo '@font/[A-Za-z0-9_]+' "$search_path" | awk -F'/' '{print $NF}'))
+  #  matches=($(grep -Eo '@'$resource_type'/[A-Za-z0-9_]+' "$search_path"))
+  list_names=($(grep -Eo '@'$resource_type'/[A-Za-z0-9_]+' "$search_path" | awk -F'/' '{print $NF}'))
   if [ -n "${list_names[*]}" ]; then
     for resource_name in "${list_names[@]}"; do
-#      resource_name=$(echo "$match" | sed -E 's/@'$resource_type'\/([A-Za-z0-9_]+)/\1/')
-#      resource_name=$(echo "$resource_name" | awk -F'/' '{print $NF}')
+      #      resource_name=$(echo "$match" | sed -E 's/@'$resource_type'\/([A-Za-z0-9_]+)/\1/')
+      #      resource_name=$(echo "$resource_name" | awk -F'/' '{print $NF}')
       path_list=$(find "$Jadx_RES_PATH" -name "$resource_name.*")
       for path in $path_list; do
         echo "path =>$path" >>path.xml
@@ -109,9 +118,10 @@ fun_main() {
   fi
 }
 #fun_main "xml" "layout" $RES_PATH
-fun_main "java" "layout" $JAVA_SRC_PATH
-#fun_main "xml"
-
+for type_name in "${directorieslist[@]}"; do
+  fun_main "java" "$type_name" $JAVA_SRC_PATH
+done
+#fun_main "java" "layout" $JAVA_SRC_PATH
 
 
 #styles_xml_file="C:/AndroidProject/Test/TypingTest/app/src/main/res/layout/activity_main.xml"
