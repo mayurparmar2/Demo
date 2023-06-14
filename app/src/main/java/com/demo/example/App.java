a3c477232b6f0b2b411518f86eb170c41034cea7
theparmarmayur <mayurparmar6000@gmail.com>
2023-06-14 11:24:45 +0530

    14 june office

--- app/src/main/java/com/demo/example/App.java
+++ app/src/main/java/com/demo/example/App.java
@@ -2,12 +2,17 @@ package com.demo.example;
 
 import android.Manifest;
 import android.app.Application;
+import android.content.Context;
 import android.os.Build;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class App extends Application {
+    public static Context getContext() {
+        return context;
+    }
+    public static Context context;
     private static final String[] REQUIRED_PERMISSIONS = getRequiredPermissions().toArray(new String[0]);
     private final String TAG = "App";
     public static List<String> getRequiredPermissions() {
@@ -28,6 +33,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        context=this;
     }
 }
 
